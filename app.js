519505e0f18a4cbeee09a1a94751c0b0b01e8cce
edgdougui_ayoub <edgdougui.ayoub@gmail.com>
2017-12-17 12:08:11 +0000

    setup passpot local Stategy and user model

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const mongoose =require('mongoose');
+const passport=require('passport');
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-
+const session =require('express-session');
 let index = require('./routes/index');
 let users = require('./routes/users');
 
+mongoose.Promise=global.Promise;
+mongoose.connect('mongodb://localhost:27017/user', {
+    useMongoClient: true,
+})
+    .then(()=>console.log('Mongodb connected'))
+    .catch(err=>console.log(err));
 let app = express();
-
+//use session
+ app.use(session({
+     secret:'key crypt',
+     resave:true,
+     saveUninitialized:true
+ }));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -23,9 +35,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', index);
 app.use('/users', users);
-
+//require local strategy passport
+require('./config/passport')(passport);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   let err = new Error('Not Found');
---
+++ config/passport.js
@@ -0,0 +1,31 @@
+let Localstrategy =require('passport-local').Strategy;
+const bcrypt = require('bcryptjs');
+
+const User = require('../public/models/users');
+module.exports=function (passport) {
+    passport.use(new LocalStrategy({usernameField: 'email'}, (email, password, done) => {
+        User.findOne({email:email})
+            .then(user=>{
+                if(!user){
+                    return done(null,false,{message:'no user found'});
+                }
+                //test if the password match
+                bcrypt.compare(password,user.password,(err,isMatch)=>{
+                    if(isMatch){
+                        return done(null,user);
+                    }else {
+                        return done(null,false,{message:'password incorrect'});
+                    }
+                });
+            })
+    }));
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser(function(id, done) {
+        User.findById(id, function(err, user) {
+            done(err, user);
+        });
+    });
+};
